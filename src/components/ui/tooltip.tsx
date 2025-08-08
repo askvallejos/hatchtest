845232cf2f97d590cb893b06612ddb3c12dc9eb5
Al Sandre Konrad B. Vallejos <alsandrevhatchitsolutions@gmail.com>
2025-08-08 09:34:59 +0800

    Refactor TooltipContent className for improved readability

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -17,9 +17,9 @@ const TooltipContent = React.forwardRef<
     <TooltipPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
-                      className={cn(
-          "z-50 overflow-visible rounded-xs bg-primary px-4 h-10 flex items-center text-sm text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-tooltip-content-transform-origin] duration-150 ease-out relative",
-          "before:absolute before:w-0 before:h-0 before:border-4 before:border-transparent before:z-10",
+      className={cn(
+        "z-50 overflow-visible rounded-xs bg-primary px-4 h-10 flex items-center text-sm text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-tooltip-content-transform-origin] duration-150 ease-out relative",
+        "before:absolute before:w-0 before:h-0 before:border-4 before:border-transparent before:z-10",
         "data-[side=top]:before:border-t-primary data-[side=top]:before:top-full data-[side=top]:before:left-1/2 data-[side=top]:before:-translate-x-1/2",
         "data-[side=bottom]:before:border-b-primary data-[side=bottom]:before:bottom-full data-[side=bottom]:before:left-1/2 data-[side=bottom]:before:-translate-x-1/2",
         "data-[side=left]:before:border-l-primary data-[side=left]:before:left-full data-[side=left]:before:top-1/2 data-[side=left]:before:-translate-y-1/2",
